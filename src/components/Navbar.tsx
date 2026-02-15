c312dfe727a04ab1f607bf13ce4263d3883614d6
VoltSMS Deploy <deploy@voltsms.com>
2026-02-16 02:37:13 +0300

    Final Fix: Resolved JSX tag mismatch in Navbar and Page layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -267,7 +267,6 @@ export default function Navbar() {
                     </motion.div>
                 )}
             </AnimatePresence>
-        </div >
-        </nav >
+        </nav>
     );
 }
